cb1c1b35d7d3ea85d021fc4e97464d416eea3d06
Andre Oliveira <andreoliveirar2d2@protonmail.com>
2021-07-02 13:42:16 -0300

    fix: putting js.txt in tmp/

--- src/search_javascripts.sh
+++ src/search_javascripts.sh
@@ -7,11 +7,11 @@ subsjs() {
   do
     if [ "$notify_enabled" == "true" ]
     then
-      cat tmp/subdomains.txt | subsjs | anew js.txt | notify --silent
-      cat tmp/subdomains.txt | gau | subsjs | anew js.txt | notify --silent
+      cat tmp/subdomains.txt | subjs | anew tmp/js.txt | notify -silent
+      cat tmp/subdomains.txt | gau | subsjs | anew tmp/js.txt | notify -silent
     else
-      cat tmp/subdomains.txt | subsjs | anew js.txt
-      cat tmp/subdomains.txt | gau | subsjs | anew js.txt
+      cat tmp/subdomains.txt | subjs | anew tmp/js.txt
+      cat tmp/subdomains.txt | gau | subsjs | anew tmp/js.txt
     fi
     sleep 3600
   done
@@ -24,9 +24,9 @@ wayback() {
   do
     if [ "$notify_enabled" == "true" ]
     then
-      cat tmp/subdomains.txt | waybackurls | grep -E "\.js(?:onp?)?$" | anew js.txt | notify --silent
+      cat tmp/subdomains.txt | waybackurls | grep -E "\.js(?:onp?)?$" | anew tmp/js.txt | notify -silent
     else
-      cat tmp/subdomains.txt | waybackurls | grep -E "\.js(?:onp?)?$" | anew js.txt
+      cat tmp/subdomains.txt | waybackurls | grep -E "\.js(?:onp?)?$" | anew tmp/js.txt
     fi
     sleep 3600
   done
@@ -39,12 +39,28 @@ gu() {
   do
     if [ "$notify_enabled" == "true" ]
     then
-      cat tmp/subdomains.txt | gau | grep -E "\.js(?:onp?)?$" | anew js.txt | notify --silent
+      cat tmp/subdomains.txt | gau | grep -E "\.js(?:onp?)?$" | anew tmp/js.txt | notify -silent
     else
-      cat tmp/subdomains.txt | gau | grep -E "\.js(?:onp?)?$" | anew js.txt
+      cat tmp/subdomains.txt | gau | grep -E "\.js(?:onp?)?$" | anew tmp/js.txt
     fi
     sleep 3600
   done
+}
+
+validateJavascripts() {
+  figlet anti-burl
+
+  file="tmp/javascripts_unresolved.txt"
+  mv tmp/js.txt $file
+
+  if [ "$notify_enabled" == "true" ]
+  then
+    cat $file | anti-burl | awk '{print $4}' | anew tmp/js.txt | notify -silent
+  else
+    cat $file | anti-burl | awk '{print $4}' | anew tmp/js.txt
+  fi
+
+  rm -rf $file
 }
 
 #### END FUNCTIONS ####
@@ -60,6 +76,8 @@ Will search by domain and write tmp/js.txt
 1 - Waybackurls
 2 - Gau
 
+97 - validate javascripts
+98 - collect info from javascripts
 99 - search by subdomains
 100 - enable notify
 "
@@ -68,13 +86,19 @@ read -p 'Choose your service: ' service
 
 case $service in
   '0' | 0)
-    subjs
+    subsjs
     ;;
   '1' | 1)
     wayback
     ;;
   '2' | 2)
     gu
+    ;;
+  '97' | 97)
+    validateJavascripts
+    ;;
+  '98' | 98)
+    collectorJavascript
     ;;
   '99' | 99)
     ./src/monitoring.sh
